b99b96e72feb0569569337cc3e275d22bed8d79f
A. J. K <wetido@gmail.com>
2023-06-11 17:22:38 +0200

    First concept of hosting nest with cors enabling and port settings.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('port');
+  const isCorsEnabled = configService.get('isCorsEnabled') === 'true';
+
+  isCorsEnabled && app.enableCors();
+
+  await app.listen(port);
 }
+
 bootstrap();
